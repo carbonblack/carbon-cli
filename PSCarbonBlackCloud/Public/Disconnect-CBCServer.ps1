6d6df6c1b827013b58fb55dbf13a59097c285f13
gmirela <gmirela@vmware.com>
2022-10-04 17:35:20 +0300

    Disconnect-CBCServer

--- PSCarbonBlackCloud/Public/Disconnect-CBCServer.ps1
+++ PSCarbonBlackCloud/Public/Disconnect-CBCServer.ps1
@@ -1,3 +1,32 @@
 function Disconnect-CBCServer {
-    Remove-Variable -Name CBC_CURRENT_SERVER -Scope Global
+    [CmdletBinding()]
+    Param(
+        [Parameter(Mandatory = $true)]
+        $Server
+    )
+
+    if ($Server -eq '*') {
+        Remove-Variable -Name CBC_CURRENT_CONNECTIONS -Scope Global
+    }
+    elseif ($Server -is [array]) {
+        $CBC_CURRENT_CONNECTIONS | ForEach-Object -Begin { $i = 0 } {
+            foreach ($server in $Server) {
+                if ($_.Server -eq $server) {
+                    $CBC_CURRENT_CONNECTIONS.RemoveAt($i)
+                    # Remove-Variable -Name $CBC_CURRENT_CONNECTIONS[$i] -Scope Global
+                }
+            }
+            $i++
+        }   
+    }
+    else {
+        $CBC_CURRENT_CONNECTIONS | ForEach-Object -Begin { $i = 0 } {
+            if ($_.Server -eq $Server) {
+                #Remove-Variable -Name $CBC_CURRENT_CONNECTIONS[$i] -Scope Global
+                $CBC_CURRENT_CONNECTIONS.RemoveAt($i)
+            }
+            $i++
+        }   
+    }
+    
 }
