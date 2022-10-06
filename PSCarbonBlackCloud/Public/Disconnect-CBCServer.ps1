ce8f00e5d9e7686dcf0ec474ef8eaf2f73126379
gmirela <gmirela@vmware.com>
2022-10-06 16:34:20 +0300

    Disconnect-CBCServer with Object

--- PSCarbonBlackCloud/Public/Disconnect-CBCServer.ps1
+++ PSCarbonBlackCloud/Public/Disconnect-CBCServer.ps1
@@ -10,19 +10,26 @@ function Disconnect-CBCServer {
     }
     elseif ($Server -is [array]) {
         $CBC_CURRENT_CONNECTIONS | ForEach-Object -Begin { $i = 0 } {
-            foreach ($server in $Server) {
-                if ($_.Server -eq $server) {
+            foreach ($s in $Server) {
+                if ($_.Server -eq $s) {
                     $CBC_CURRENT_CONNECTIONS.RemoveAt($i)
-                    # Remove-Variable -Name $CBC_CURRENT_CONNECTIONS[$i] -Scope Global
                 }
             }
             $i++
         }   
     }
+    elseif($Server -is [System.Management.Automation.PSCustomObject])
+    {
+        $CBC_CURRENT_CONNECTIONS | ForEach-Object -Begin { $i = 0 } {
+            if ($_.Server -eq $Server.server) {
+                $CBC_CURRENT_CONNECTIONS.RemoveAt($i)
+            }
+            $i++
+        }
+    }
     else {
         $CBC_CURRENT_CONNECTIONS | ForEach-Object -Begin { $i = 0 } {
             if ($_.Server -eq $Server) {
-                #Remove-Variable -Name $CBC_CURRENT_CONNECTIONS[$i] -Scope Global
                 $CBC_CURRENT_CONNECTIONS.RemoveAt($i)
             }
             $i++
