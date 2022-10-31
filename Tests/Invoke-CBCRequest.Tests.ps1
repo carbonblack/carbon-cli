faf7f1bfc856c20dd56a91056d91926cd0dda4dc
gmirela <gmirela@vmware.com>
2022-10-31 17:11:50 +0200

    Refactored Invoke-CBCRequest.ps1

--- Tests/Invoke-CBCRequest.Tests.ps1
+++ Tests/Invoke-CBCRequest.Tests.ps1
@@ -15,7 +15,7 @@ InModuleScope PSCarbonBlackCloud {
         BeforeAll {
 
             $TestServerObject1 = @{
-                Uri   = "https://test.adasdagf/"
+                Uri   = "https://defense-prod05.conferdeploy.net/"
                 Org   = "test"
                 Token = "test"
             }
@@ -36,23 +36,25 @@ InModuleScope PSCarbonBlackCloud {
         # Resetting the State of tests
         BeforeEach {
             $CBC_CONFIG.currentConnections = [System.Collections.ArrayList]@()
+            Disconnect-CBCServer *
         }
 
         It "Should return status 200" {
+            
+            $CBC_CONFIG.currentConnections.Add($TestServerObject1)
 
             Mock -ModuleName PSCarbonBlackCloud Invoke-WebRequest -MockWith {
                 return @{
                     StatusCode = 200
                 }
             } -ParameterFilter { 
-                $Uri -match "https://test.adasdagf/appservices/v6/orgs/test/devices/123" 
-                $Method -match "Get" 
-                $Params -match @(123)
+                $Uri -match "https://defense-prod05.conferdeploy.net/appservices/v6/orgs/test/devices/123"
             }
+            $url = "appservices/v6/orgs/{0}/devices/{1}"
 
-            $CBC_CONFIG.currentConnections.Add($TestServerObject1)
+            $response = Invoke-CBCRequest -Uri $url -Method "Get" -Params @("123")
 
-            Invoke-CBCRequest -Uri $CBC_CONFIG.endpoints["Devices"]["SpecDevInfo"] -Method "Get" -Params @(123)
+            $response | Should -Not -Be $null
         }
 
     }
--- src/Private/Invoke-CBCRequest.ps1
+++ src/Private/Invoke-CBCRequest.ps1
@@ -1,10 +1,4 @@
 function Invoke-CBCRequest {
-    <#
-    .ForwardHelpTargetName
-        Microsoft.PowerShell.Utility\Invoke-WebRequest
-    .ForwardHelpCategory
-        Cmdlet
-    #>
     [CmdletBinding()]
     Param(
         [Parameter(Mandatory = $true, Position = 0)]
@@ -12,46 +6,11 @@ function Invoke-CBCRequest {
         [string] $Uri, 
 
         [Parameter(Mandatory = $true, Position = 1)]
-        [Microsoft.PowerShell.Commands.WebRequestMethod] $Method,
+        [string] $Method,
 
         [array] $Params,
 
-        [ValidateNotNull()]
-        [X509Certificate] $Certificate,
-
-        [switch] $SkipCertificateCheck,
-
-        [System.Collections.IDictionary] ${Headers},
-
-        [Microsoft.PowerShell.Commands.WebSslProtocol] $SslProtocol,
-
-        [string] $UserAgent,
-
-        [switch] $DisableKeepAlive,
-
-        [ValidateRange(0, 20)]
-        [int32] $TimeoutSec,
-
-        [ValidateRange(0, 10)]
-        [int] $MaximumRedirection,
-
-        [switch] $NoProxy,
-
-        [uri] $Proxy,
-
-        [pscredential]
-        [System.Management.Automation.CredentialAttribute()] $ProxyCredential,
-
-        [switch] $ProxyUseDefaultCredentials,
-
-        [System.Object] $Body,
-
-        [string] $ContentType,
-
-        [ValidateSet('chunked', 'compress', 'deflate', 'gzip', 'identity')]
-        [string] $TransferEncoding,
-
-        [switch] $SkipHeaderValidation
+        [System.Object] $Body
     )
     
     Process {
@@ -59,19 +18,18 @@ function Invoke-CBCRequest {
         $requestObjects = [System.Collections.ArrayList]@()
     
         $CBC_CONFIG.currentConnections | ForEach-Object {
-            $PSBoundParameters["Headers"] = @{}
-            $PSBoundParameters["Headers"]["X-AUTH-TOKEN"] = $_.Token
-            $PSBoundParameters["Headers"]["Content-Type"] = "application/json"
-            $PSBoundParameters["Headers"]["User-Agent"] = "PSCarbonBlackCloud"
-            $PSBoundParameters["Body"] = $PSBoundParameters["Body"] | ConvertTo-Json
-            $PSBoundParameters["Uri"] = ($_.Uri + $Uri) -f $_.Org, ($Params -Join ',')
-            $PSBoundParameters.Remove("Params")
-            $response = Invoke-WebRequest @PSBoundParameters
-            $requestObjects.Add($response) | Out-Null
+            $headers = [System.Collections.IDictionary]@{}
+            $headers["X-AUTH-TOKEN"] = $_.Token
+            $headers["Content-Type"] = "application/json"
+            $headers["User-Agent"] = "PSCarbonBlackCloud"
+           
+            $Params = ,$_.Org + $Params
+            $formatted_uri = $Uri -f $Params
+
+            $FullUri = $_.Uri + $formatted_uri
+            $response = Invoke-WebRequest -Uri $FullUri -Headers $headers -Method $Method
+            $requestObjects.Add(@{$_.Org = $response}) | Out-Null
         }
-
         $requestObjects
-
-    }
-    
+    }  
 }
