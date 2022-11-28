d642d59fedd9cb6c98e97b5293eaee0e9c7d3843
gmirela <gmirela@vmware.com>
2022-11-28 10:08:07 +0200

    Fix bug with Save Credentials

--- src/PSCarbonBlackCloud.Classes.psm1
+++ src/PSCarbonBlackCloud.Classes.psm1
@@ -226,13 +226,6 @@ class CBCServer {
         return "[" + $this.Org + "] " + $this.Uri
     }
 }
-class Rule {
-    [long]$Id
-    [switch]$Required
-    [string]$Action
-    [hashtable]$Application
-    [string]$Operation
-}
 
 class Policy {
     [long]$Id
@@ -245,7 +238,7 @@ class Policy {
     [long]$AutoDeregisterInactiveVdiIntervalMs
     [long]$AutoDeleteKnownBadHashesDelay
     [hashtable]$AvSettings
-    [Rule[]]$Rules
+    [hashtable[]]$Rules
     [long]$AutoDeregisterInactiveVmWorkloadsIntervalMs
     [hashtable[]]$DirectoryActionRules
     [string[]]$RapidConfigs
--- src/Private/Save-CBCCredential.ps1
+++ src/Private/Save-CBCCredential.ps1
@@ -6,16 +6,34 @@ function Save-CBCCredential {
 
     Process {
         $credentialsDoc = New-Object System.Xml.XmlDocument
-        $credentialsDoc.Load($CBC_CONFIG.credentialsFullPath)
+        if(Test-Path -Path $CBC_CONFIG.credentialsFullPath) {
+            $credentialsDoc.Load($CBC_CONFIG.credentialsFullPath)
+        }
+        else {
+            New-Item -Path $CBC_CONFIG.credentialsFullPath
+            # Add root element
+            Add-Content -Path $CBC_CONFIG.credentialsFullPath "<Servers>"
+            Add-Content -Path $CBC_CONFIG.credentialsFullPath "</Servers>"
+            # Load file
+            $credentialsDoc.Load($CBC_CONFIG.credentialsFullPath)
+        }
 
         $serverElement = $credentialsDoc.CreateElement("Server")
         $serverElement.SetAttribute("Uri", $ServerObject.Uri)
         $serverElement.SetAttribute("Org", $ServerObject.Org)
         $serverElement.SetAttribute("Token", $ServerObject.Token)
 
-        $serversNode = $credentialsDoc.SelectSingleNode("Servers")
-        $serversNode.AppendChild($serverElement)
+        # $root = $credentialsDoc.DocumentElement;
 
+        $serversNode = $credentialsDoc.SelectSingleNode("Servers")
+        #$uri = $ServerObject.Uri
+        #$serverNode = $credentialsDoc.SelectSingleNode("//Server[@Uri=${uri}]")
+        #if ($serverNode -eq $null) {
+        #    $serversNode.AppendChild($serverElement)
+        #} else {
+        #    Write-Host "Found it"
+        # }
+        $serversNode.AppendChild($serverElement)
         $credentialsDoc.Save($CBC_CONFIG.credentialsFullPath)
     }
 }
