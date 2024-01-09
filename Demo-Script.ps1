42966d9868b3499ceb5b8e95ae913b4a2c2b10c1
Emanuela Mitreva <emitreva@vmware.com>
2024-01-09 17:27:50 +0200

    Add more examples in the demo-script

--- Demo-Script.ps1
+++ Demo-Script.ps1
@@ -11,7 +11,7 @@ Get-Command -Module PSCarbonBlackCloud | Measure-Object | Select Count
 # Establish a connection to a Carbon Black Cloud Endpoint
 # Create a PSCredential object. Specify OrgId as a value for User and Token as a value for password when prompted.
 $myCredentials = Get-Credential
-$cbcServerOrg1 = Connect-CBCServer -Server https://defense-dev01.cbdtest.io/ -Credential $myCredentials -Notes Org1
+$cbcServerOrg1 = Connect-CBCServer -Server https://defense.conferdeploy.net/ -Credential $myCredentials -Notes Org1
 
 
 # Design principle on focus: Each cmdlet is documented as part of its codebase. Learn Carbon Black leveraging Get-Help cmdlet
@@ -32,7 +32,7 @@ Get-CbcProcess | Get-Member
 Get-CbcDevice | Select-Object Id, Server, LastShutdownTime
 
 # Design principle on focus: Working against multiple connections. Each connection is defined uniquely by a URI/Org pair. 
-$cbcServerOrg2 = Connect-CBCServer -Server https://defense-test03.cbdtest.io/ -Org Org2Id -Token WC7LDKCJV6ZN69AD5P7ZQR3/QCHECHM3QE -Notes Org2
+$cbcServerOrg2 = Connect-CBCServer -Server https://defense.conferdeploy.net/ -Org ABCD1234 -Token ABCDEFGHIJKLMNO123456789/ABCD123456 -Notes Org2
 
 
 # Retrieve alerts, devices from multiple connections.
@@ -59,9 +59,14 @@ Get-CbcDevice -OS Windows -OSVersion "Windows 10 x64" -TargetPriority LOW | Set-
 Get-CbcDevice -OS Windows -OSVersion "Windows 10 x64" -TargetPriority LOW | Set-CbcDevice -QuarantineEnabled $false
 Get-Help Set-CbcDevice -Full
 
+# Get policies and update device policy
+Get-CbcDevice
+Get-CbcPolicy
+Set-CbcDevice -Id 123456789 -PolicyId 12345
+
 # Working with long running operations ( process search / observations search )
 Get-CbcDevice 
-Get-CbcProcess -DeviceId
+Get-CbcProcess -DeviceId 123456789
 Get-CbcProcess -Query "process_name:power*" 
 $processSearchJob = Get-CbcProcess -Query "process_name:power*" -AsJob
 Get-CbcJob $processSearchJob
@@ -72,3 +77,21 @@ Get-Help Get-CbcObservation -Full
 Get-CbcObservation -EventType "netconn"
 Get-CbcObservation -Query "event_type:netconn"
 
+# Get observations for alert
+Get-CbcAlert -Id "c295b8fc-0f5d-7193-3075-0301862c69b7" | Get-CbcObservation
+
+# Create feed, report, ioc and then subscribe for that feed (create a watchlist)
+# Create empty feed
+$feed = New-CbcFeed -Name mydemo -ProviderUrl http://test.test/ -Summary summary -Category demo -Alertable $true
+
+# Create empty report and add it to the feeed
+$report = New-CbcReport -Feed $feed -Title myreport -Description description -Severity 5
+
+# Create one IOC and add it to the created report
+$Body = @{"match_type" = "equality"
+"field" =  "process_name"
+"values" = @("googleupdate.exe")}
+New-CbcIoc -Report $report -Body $Body
+
+# Subscribe to that feed
+New-CbcWatchlist -Feed $feed
