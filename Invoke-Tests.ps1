342c488dd5f580445535fc14954d486f8ffa5cf5
Alex Van Brunt <alex.van-brunt@broadcom.com>
2024-09-24 15:28:16 -0600

    Add sonarqube

--- Invoke-Tests.ps1
+++ Invoke-Tests.ps1
@@ -1,1 +1,8 @@
-Invoke-Pester -Path .\Tests\* -Output Detailed -WarningAction Ignore -CI
+$config = New-PesterConfiguration
+$config.CodeCoverage.OutputFormat = 'CoverageGutters'
+$config.CodeCoverage.OutputPath = 'cov.xml'
+$config.CodeCoverage.OutputEncoding = 'UTF8'
+
+$config.CodeCoverage.Enabled = $true
+
+Invoke-Pester -Configuration $config -Path .\Tests\* -Output Detailed -WarningAction Ignore -CI
